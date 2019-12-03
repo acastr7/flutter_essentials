89307343d3c6b48ada0a7330f86c3de3259e6f3d
Andres Castro <acastr7@gmail.com>
2019-12-02 21:01:52 -0500

    iOS implementation

--- ios/Classes/SwiftFlutterEssentialsPlugin.swift
+++ ios/Classes/SwiftFlutterEssentialsPlugin.swift
@@ -2,10 +2,13 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterEssentialsPlugin: NSObject, FlutterPlugin {
+  static var accelerometer : Accelerometer?
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_essentials", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterEssentialsPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    SwiftFlutterEssentialsPlugin.accelerometer = Accelerometer(messenger: registrar.messenger())
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -20,6 +23,9 @@ public class SwiftFlutterEssentialsPlugin: NSObject, FlutterPlugin {
     }
     else if call.method.starts(with: PhoneDialer.prefix){
         PhoneDialer.handle(call, result: result)
+    }
+    else if call.method.starts(with: Accelerometer.prefix){
+        SwiftFlutterEssentialsPlugin.accelerometer?.handle(call, result: result)
     }
     else{
         result(FlutterMethodNotImplemented)
